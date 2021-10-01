f2e820f80017cb095f034c9407ecf77e4e11ed28
Flavien Lallemand <flavien@flmd.fr>
2021-10-01 20:12:06 +0200

    Add Widget Embded as component

--- part_3_cache.py
+++ part_3_cache.py
@@ -213,6 +213,9 @@ components.html(
     height=600,
 )
 
+components.iframe("https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik")
+
+
 if option == 'Uber April 2014':
 
 
